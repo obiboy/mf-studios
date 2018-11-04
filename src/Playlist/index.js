6bf647d3994c79d5aa795272e2c9a24dca1fa4c6
obiboy <obiboy19@gmail.com>
2018-11-04 08:39:34 -0600

    all nighter

--- src/Playlist/index.js
+++ src/Playlist/index.js
@@ -7,40 +7,75 @@ export function Playlist (props) {
         props.playlists.map((playlist, id) => (
           <div>
             <div style={{
-              borderLeft: '1px solid blue',
+              borderLeft: '3px solid blue',
               position: 'absolute',
-              top: '17.5vw',
-              left: 'calc(13.1vw)',
+              top: '22.5vw',
+              left: 'calc(14.4vw)',
               fontSize: '100vh',
-            }}>&nbsp;</div>
+            }}>&nbsp;
+            </div>
             <div style={{
               float: 'left',
-              width: '3vw',
-              height: '3vw',
+              width: '5vw',
+              height: '5vw',
               display: 'flex',
               alignItems: 'center',
               justifyContent: 'center',
               border: '1px solid black'
-            }}>{id + 1}</div>
+            }}>{id + 1}
+            </div>
             <div style={{
               float: 'left',
-              width: '74vw',
-              height: '3vw',
+              width: '72vw',
+              height: '5vw',
               background: 'red',
               border: '1px solid black',
               display: 'flex',
-              alignItems: 'center'
-            }}>
-              {
-                playlist.sequence.map(id => {
-                  const pattern = props.patterns[id]
-                  if (!pattern) {
-                    return (<div style={{width: '5vw', height: '3vw', borderRight: '1px solid gray'}}>&nbsp;</div>)
-                  } else {
-                    return (<div style={{width: '5vw', height: '3vw', borderRight: '1px solid gray'}}>Pattern {id + 1}</div>)
-                  }
-                })
-              }
+              alignItems: 'left'
+
+            }} class="grid-container">
+            <div class="grid-item" style={{
+              width: '10%',
+              height: '95%',
+              border: '2px solid black'
+            }}>1</div>
+            <div class="grid-item" style={{
+              width: '10%',
+              height: '95%',
+              border: '2px solid black'
+            }}>2</div><div class="grid-item" style={{
+              width: '10%',
+              height: '95%',
+              border: '2px solid black'
+            }}>3</div><div class="grid-item" style={{
+              width: '10%',
+              height: '95%',
+              border: '2px solid black'
+            }}>4</div><div class="grid-item" style={{
+              width: '10%',
+              height: '95%',
+              border: '2px solid black'
+            }}>5</div><div class="grid-item" style={{
+              width: '10%',
+              height: '95%',
+              border: '2px solid black'
+            }}>6</div><div class="grid-item" style={{
+              width: '10%',
+              height: '95%',
+              border: '2px solid black'
+            }}>7</div><div class="grid-item" style={{
+              width: '10%',
+              height: '95%',
+              border: '2px solid black'
+            }}>8</div><div class="grid-item" style={{
+              width: '10%',
+              height: '95%',
+              border: '2px solid black'
+            }}>9</div><div class="grid-item" style={{
+              width: '10%',
+              height: '95%',
+              border: '2px solid black'
+            }}>10</div>
             </div>
             <div style={{clear: 'both'}}></div>
           </div>
