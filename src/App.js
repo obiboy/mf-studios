7575642008f31950b3543edf879d4921cfa0105d
James Costian <james@jamescostian.com>
2018-11-03 19:59:55 -0500

    Some shit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,60 @@
 import React, { Component } from 'react'
 import { MFControls } from './MFControls'
+import { MixerRack } from './MixerRack'
+import { Playlist } from './Playlist'
+import { FileList } from './FileList'
 import './App.css'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      playing: false,
+      recording: false, // or something like {start: 20} if it is actually recording
+      time: 0,
+      bpm: 110
+    }
+  }
+  pause() {
+    // TODO
+    this.setState(state => ({playing: false}))
+  }
+  play() {
+    // TODO
+    this.setState(state => ({playing: true}))
+  }  stopRecording() {
+    // TODO
+    this.setState(state => ({recording: false}))
+  }
+  startRecording() {
+    // TODO
+    this.setState(state => ({recording: {start: state.time}}))
+  }
+  setTime(time) {
+    this.setState({time})
+  }
+  setBPM(bpm) {
+    // TODO
+    this.setState({bpm})
+  }
   render() {
     return (
       <div className="App">
-        <MFControls></MFControls>
+        <MFControls
+          playing={this.state.playing}
+          recording={this.state.recording}
+          bpm={this.state.bpm}
+          updateBPM={this.state.updateBPM}
+          togglePlaying={() => this.state.playing ? this.pause() : this.play()}
+          toggleRecording={() => {this.state.recording ? this.stopRecording() : this.startRecording()}}
+          stop={() => {
+            this.pause();
+            this.setTime(0)
+          }}>
+        </MFControls>
+        <MixerRack></MixerRack>
+        <Playlist></Playlist>
+        <FileList></FileList>
       </div>
     )
   }
--- src/MFControls/index.js
+++ src/MFControls/index.js
@@ -4,16 +4,20 @@ import pauseIcon from '../material/pause.svg'
 import stopIcon from '../material/stop.svg'
 import micIcon from '../material/mic.svg'
 
+// Sorry
+const makeMySVGRed = {filter: 'invert(0.3) sepia(1) saturate(10) hue-rotate(-30deg)'}
+
 export function MFControls (props) {
   return (<div style={{textAlign: 'center', padding: '2vh 0', borderBottom: '1px solid black'}}>
-    <a href="#">
-      <img src={props.playing ? playArrowIcon : pauseIcon} />
+    <a href="#" onClick={props.togglePlaying}>
+      <img src={props.playing ? pauseIcon : playArrowIcon} />
     </a>
-    <a href="#">
+    <a href="#" onClick={props.stop}>
       <img src={stopIcon} />
     </a>
-    <a href="#" style={{color: props.recording ? 'red' : 'inherit'}}>
+    <a href="#" onClick={props.toggleRecording} style={props.recording ? makeMySVGRed : {}}>
       <img src={micIcon} />
     </a>
+    <input onChange={e => props.updateBPM(e.target.value)} value={props.bpm} cols="3" />
   </div>)
 }
